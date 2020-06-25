d14d2fea5071b44a89f213bbb8a401c5463d5869
Gabriela Possari <gabrielapossari@hotmail.com>
2020-06-25 19:03:51 -0300

    gabriela

--- WSTower.WebApi/Domains/Usuario.cs
+++ WSTower.WebApi/Domains/Usuario.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace WSTower.WebApi.Domains
 {
     public partial class Usuario
     {
+
         public int Id { get; set; }
-        public string Nome { get; set; }
+        // Define que o nome é obrigatória
+        [Required(ErrorMessage = "Informe seu Nome")]
+        // Define o tipo do dado
+        [DataType(DataType.Text)]
+        public string Nome  { get; set; }
+        // Define que o email é obrigatória
+        [Required(ErrorMessage = "Informe seu email")]
+        // Define o tipo do dado
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        // Define que o nome é obrigatória
+        [Required(ErrorMessage = "Informe seu apelido")]
+        // Define o tipo do dado
+        [DataType(DataType.Text)]
         public string Apelido { get; set; }
+        // Define que o nome é obrigatória
+        [Required(ErrorMessage = "Informe sua foto")]
+        // Define o tipo do dado
+        [DataType(DataType.ImageUrl)]
         public byte[] Foto { get; set; }
+        // Define que o nome é obrigatória
+        [Required(ErrorMessage = "Informe sua senha")]
+        // Define o tipo do dado
+        [DataType(DataType.Password)]
         public string Senha { get; set; }
     }
 }
